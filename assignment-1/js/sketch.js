e792a150124f4edc4d0c2792755c1e7912142c7f
JacobFerber <jferber@live.com>
2023-01-18 20:02:07 -0700

    Final Deploy

--- assignment-1/js/sketch.js
+++ assignment-1/js/sketch.js
@@ -1,3 +1,23 @@
+    var sandox= 350;
+    var sandoy= 300;
+    var sando1x= 346;
+    var sando1y= 304;
+   var tomatox=450;
+   var tomatoy=400;
+
+    var applex= 575;
+    var appley= 525;
+    var stemx=575;
+    var stemy=520   ;
+
+    var carrot1x= 320;
+    var carrot1y= 525;
+    
+    var carrot2x= 300;
+    var carrot2y=525;
+
+    var carrot3x=340;
+    var carrot3y=525;
 
     function setup()
     {
@@ -6,8 +26,30 @@
     function draw()
     {
         background(10,130,180)
+        fill(360,360,360)
         circle(450,450,450)
-        fill(4)
-        rectangle(200,300,20,30)
+        fill(240,248,255)
+        circle(450,450,420)
+
+        fill(179,110,0)
+        rect(sando1x,sando1y,200,150)
+        fill(204,136,0)
+        rect(sandox,sandoy,200,150)
+        fill(0,153,25)
+        triangle(480,325,350,375,325,325)
+        fill(255,25,25)
+        circle(tomatox,tomatoy,125)
+        fill(204,136,0)
+        rect(sandox,sandoy,200,150)
+        
+        fill(179,0,0)
+        circle(applex,appley,100)
+        fill(9,51,0)
+        rect(stemx,stemy,8,20)
+        
+        fill(255,166,77)
+        ellipse(carrot1x,carrot1y,20,100)
+        ellipse(carrot2x,carrot2y,20,100)
+        ellipse(carrot3x,carrot3y,20,100)
     }
      
