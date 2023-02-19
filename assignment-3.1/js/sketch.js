83ca5d2e61281f53fab05487b2bfbb800c745ef5
JacobFerber <jferber@live.com>
2023-02-19 14:01:36 -0700

    upload

--- assignment-3.1/js/sketch.js
+++ assignment-3.1/js/sketch.js
@@ -1,11 +1,44 @@
-var img;
+var img1;
+var img2;
+var img3;
+var timerValue = 10;
 
-function setupImage()
+function preload()
 {
-    img = loadImage('/assignment-3.1/assets/image1.jpg')
+    img1 = loadImage('./assets/image1.jpg');
+    img2 = loadImage('./assets/image2.jpg');
+    img3 = loadImage('./assets/image3.jpg');
+
 }
 function setup()
 {
-    loadImage(img)
     
+    createCanvas(250,250);
+}
+function draw()
+{
+    cassens();
+    background(img1);
+    timeIt();
+}
+function cassens()
+{
+    console.log(timerValue)
+
+background();
+  if (timerValue >= 1000) {
+    Image(img1);
+  }
+  if (timerValue < 1000) {
+    Image(img2 );
+  }
+  if (timerValue == 0) {
+    Image('game over', width / 2, height / 2 + 15);
+  }
+}
+
+function timeIt() {
+  if (timerValue > 0) {
+    timerValue--;
+  }
 }
---
+++ assignment-4/js/sketch.js
@@ -0,0 +1,12 @@
+var indie;
+var animation;
+var runAnimation;
+var indieObjects;
+var indieRunObjects;
+var i;
+
+
+function preload()
+{
+    indie = new imageClass('assets/Idle__000.png',0,0)
+}
